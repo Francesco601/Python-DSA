55ebb01a4dab9c211b7ef3f2e4d9d04876d9c6f3
Francesco Franco <130352141+Francesco601@users.noreply.github.com>
2023-06-22 10:25:07 +0200

    Traveling Salesman part 5
    Traveling salesman problem implemented in Python using
    a genetic algorithm.

---
+++ traveling_salesman5.py
@@ -0,0 +1,230 @@
+# Python implementation of solution to the Traveling
+# Salesman Problem using a genetic algorithm (GA).
+# Genetic algorithms are heuristic search algorithms
+# inspired by the process of random mutation followed
+# by evolution by natural selection. The algorithm
+# is designed to replicate the natural selection
+# process through suceeding generations. Standard
+# genetic algorithms are divided into five phases:
+# namely:
+# 1) Creating initial population.
+# 2) Calculating fitness,
+# 3) Selecting the best genes.
+# 4) Crosssing over.
+# 5) Mutating to introduce variations.
+# These algorithms can be implemented to find a
+# solution to various kinds of optimization problems
+# such as the TSP.
+#
+# In the following implementation, cities are taken
+# as genes, the strings generated using these characters
+# are called chromosomes, while a fitness score which
+# is equal to the path length of all the cities mentioned
+# is used to target a population. The Fitness score is
+# defined as the length of the path described  by the gene.
+# The smaller the path length, the fitter the gene. The
+# fittest of all the genes in the gene pool survive the
+# population test and move to the next iteration. The number
+# of iterations depends on the value of the cooling variable.
+# The value of the cooling variable keeps decreasing with
+# each iteration and reaches a threshold after a certain
+# number of iterations. The time complexity is O(n^2) since
+# two nested loops are used to calculate the fitness value
+# of each gnome in the population.
+
+
+from random import randint
+
+INT_MAX = 2147483647
+# Number of cities in TSP
+V = 5
+
+# Names of the cities
+GENES = "ABCDE"
+
+# Starting Node Value
+START = 0
+
+# Initial population size for the algorithm
+POP_SIZE = 10
+
+# Structure of a GNOME
+# defines the path traversed
+# by the salesman while the fitness value
+# of the path is stored in an integer
+
+
+class individual:
+	def __init__(self) -> None:
+		self.gnome = ""
+		self.fitness = 0
+
+	def __lt__(self, other):
+		return self.fitness < other.fitness
+
+	def __gt__(self, other):
+		return self.fitness > other.fitness
+
+
+# Function to return a random number
+# from start and end
+def rand_num(start, end):
+	return randint(start, end-1)
+
+
+# Function to check if the character
+# has already occurred in the string
+def repeat(s, ch):
+	for i in range(len(s)):
+		if s[i] == ch:
+			return True
+
+	return False
+
+
+# Function to return a mutated GNOME
+# Mutated GNOME is a string
+# with a random interchange
+# of two genes to create variation in species
+def mutatedGene(gnome):
+	gnome = list(gnome)
+	while True:
+		r = rand_num(1, V)
+		r1 = rand_num(1, V)
+		if r1 != r:
+			temp = gnome[r]
+			gnome[r] = gnome[r1]
+			gnome[r1] = temp
+			break
+	return ''.join(gnome)
+
+
+# Function to return a valid GNOME string
+# required to create the population
+def create_gnome():
+	gnome = "0"
+	while True:
+		if len(gnome) == V:
+			gnome += gnome[0]
+			break
+
+		temp = rand_num(1, V)
+		if not repeat(gnome, chr(temp + 48)):
+			gnome += chr(temp + 48)
+
+	return gnome
+
+
+# Function to return the fitness value of a gnome.
+# The fitness value is the path length
+# of the path represented by the GNOME.
+def cal_fitness(gnome):
+	mp = [
+		[0, 2, INT_MAX, 12, 5],
+		[2, 0, 4, 8, INT_MAX],
+		[INT_MAX, 4, 0, 3, 3],
+		[12, 8, 3, 0, 10],
+		[5, INT_MAX, 3, 10, 0],
+	]
+	f = 0
+	for i in range(len(gnome) - 1):
+		if mp[ord(gnome[i]) - 48][ord(gnome[i + 1]) - 48] == INT_MAX:
+			return INT_MAX
+		f += mp[ord(gnome[i]) - 48][ord(gnome[i + 1]) - 48]
+
+	return f
+
+
+# Function to return the updated value
+# of the cooling element.
+def cooldown(temp):
+	return (90 * temp) / 100
+
+
+# Comparator for GNOME struct.
+# def lessthan(individual t1,
+#			 individual t2)
+# :
+#	 return t1.fitness < t2.fitness
+
+
+# Utility function for TSP problem.
+def TSPUtil(mp):
+	# Generation Number
+	gen = 1
+	# Number of Gene Iterations
+	gen_thres = 5
+
+	population = []
+	temp = individual()
+
+	# Populating the GNOME pool.
+	for i in range(POP_SIZE):
+		temp.gnome = create_gnome()
+		temp.fitness = cal_fitness(temp.gnome)
+		population.append(temp)
+
+	print("\nInitial population: \nGNOME	 FITNESS VALUE\n")
+	for i in range(POP_SIZE):
+		print(population[i].gnome, population[i].fitness)
+	print()
+
+	found = False
+	temperature = 10000
+
+	# Iteration to perform
+	# population crossing and gene mutation.
+	while temperature > 1000 and gen <= gen_thres:
+		population.sort()
+		print("\nCurrent temp: ", temperature)
+		new_population = []
+
+		for i in range(POP_SIZE):
+			p1 = population[i]
+
+			while True:
+				new_g = mutatedGene(p1.gnome)
+				new_gnome = individual()
+				new_gnome.gnome = new_g
+				new_gnome.fitness = cal_fitness(new_gnome.gnome)
+
+				if new_gnome.fitness <= population[i].fitness:
+					new_population.append(new_gnome)
+					break
+
+				else:
+
+					# Accepting the rejected children at
+					# a possible probability above threshold.
+					prob = pow(
+						2.7,
+						-1
+						* (
+							(float)(new_gnome.fitness - population[i].fitness)
+							/ temperature
+						),
+					)
+					if prob > 0.5:
+						new_population.append(new_gnome)
+						break
+
+		temperature = cooldown(temperature)
+		population = new_population
+		print("Generation", gen)
+		print("GNOME	 FITNESS VALUE")
+
+		for i in range(POP_SIZE):
+			print(population[i].gnome, population[i].fitness)
+		gen += 1
+
+
+if __name__ == "__main__":
+
+	mp = [
+		[0, 2, INT_MAX, 12, 5],
+		[2, 0, 4, 8, INT_MAX],
+		[INT_MAX, 4, 0, 3, 3],
+		[12, 8, 3, 0, 10],
+		[5, INT_MAX, 3, 10, 0],
+	]
+	TSPUtil(mp)
